4a5976c5ed3d25443490b0f80ea86c984ba6f705
Samuel-cyber-droid <78009369+Samuel-cyber-droid@users.noreply.github.com>
2024-11-19 22:18:48 -0600

    Create constantes.py

---
+++ constantes.py
@@ -0,0 +1,28 @@
+# detalles de ventana
+ANCHO_VENTANA = 800
+ALTO_VENTANA = 600
+
+# detalles de personaje
+#ALTO_PERSONAJE = 20
+#ANCHO_PERSONAJE = 20
+COLOR_PERSONAJE = (255, 255, 0)
+SCALA_PERSONAJE = 1
+
+# color de fondo
+COLOR_BG = (0, 0, 20)
+
+# fotograma
+FPS = 60
+
+# movimiento
+VELOCIDAD_MOVIMIENTO = 5
+
+# arma
+SCALA_ARMA = 0.07
+SCALA_BALA = 0.2
+COOLDOWN_BALAS = 500
+VELOCIDAD_BALA =20
+
+#enemigos
+SCALA_ENEMIGO = 1.5
+COLOR = (203, 50, 52)
